ac43c0059eb41f5112fc6a4ee5efc08f239e67b8
Austin Chao <austinschao@gmail.com>
2022-03-22 16:44:02 -0700

    Completed serial.py

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,25 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    #static variable
+
+    start_number = 0 #100 => 101 => 102
+
+    def __init__(self, start):
+        """Constructing a serial generator"""
+        self.start = start
+        SerialGenerator.start_number = start
+
+
+    def generate(self):
+        """Incrementing start number by 1"""
+        if SerialGenerator.start_number == self.start:
+            SerialGenerator.start_number += 1
+            return self.start
+        SerialGenerator.start_number += 1
+        return SerialGenerator.start_number - 1
+
+    def reset(self):
+        """Reset start number to original val"""
+        SerialGenerator.start_number = self.start
